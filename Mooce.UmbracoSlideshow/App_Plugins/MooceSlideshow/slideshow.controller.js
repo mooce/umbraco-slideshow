0e741355040b7e411f120d18a86bc6413427f2bf
Dacre <enquiries@mooce.co.nz>
2018-07-22 20:48:32 +1200

    wiring up of basic add/remove slide logic, picker drawers partially wired up with model view

--- Mooce.UmbracoSlideshow/App_Plugins/MooceSlideshow/slideshow.controller.js
+++ Mooce.UmbracoSlideshow/App_Plugins/MooceSlideshow/slideshow.controller.js
@@ -4,15 +4,71 @@ angular.module("umbraco")
         
         // assetsService.loadCss("/App_Plugins/styles.css");
 
+        $scope.model.value = Object.assign($scope.model.value, {
+            slideDuration : 1000,
+            slides : []
+        })
+
+
+
         $scope.current = {
+            index : 0,
             backgroundUrl:'',
             heading:'',
             caption:'',
-            action:''
+            link:''
         }
 
-        $scope.pickPhoto = function(slide) {
+        $scope.getAllSlides = function() {
 
-            dialogService.mediaPicker({ onlyImages : true })
+            return $scope.model.value.slides
+        }
+
+        $scope.getCurrentSlide = function() {
+
+            return $scope.current
+        }
+
+        $scope.deleteSlide = function($event, slide) {
+            $event.preventDefault();
+
+            var value = $scope.model.value;
+            var index = value.slides.indexOf(slide);
+
+            if(index !== -1) {
+                value.slides.splice(index, 1)
+            }
+
+
+        }
+
+        $scope.addSlide = function($event) {
+            $event.preventDefault();
+            $scope.model.value.slides.push({
+                index : $scope.model.value.slides.length,
+                backgroundUrl:'',
+                heading:'',
+                caption:'',
+                link:''
+            })
+        }
+
+        $scope.pickPhoto = function($event, slide) {
+
+            $event.preventDefault();
+            dialogService.mediaPicker({ onlyImages : true, callback : function(media) {
+
+                $scope.current.backgroundUrl = media.image
+            }})
+        } 
+
+        $scope.pickLink = function($event, slide) {
+
+            $event.preventDefault();
+            dialogService.linkPicker({ onlyImages : true, callback : function(item) {
+
+                debugger
+                $scope.current.link = item.url
+            }})
         }         
     }]);
